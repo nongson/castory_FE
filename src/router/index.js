c1b25db73ad62255a0ecc99bf650ea461b30b42b
Internship-Thai Quang Minh <minhtq@vsii.com>
2023-04-26 10:28:39 +0700

    protect-router

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,14 @@ import VueRouter from "vue-router";
 import LoginView from "@/views/auth/LoginView.vue";
 import CardView from "@/views/card/CardView.vue";
 import ListView from "@/views/list/ListView.vue";
+import store from "@/store";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    component: ListView,
+    redirect: "/list",
   },
   {
     path: "/login",
@@ -20,11 +21,14 @@ const routes = [
     path: "/list",
     name: "list",
     component: ListView,
+    meta: { needLogin: true },
   },
   {
     path: "/list/:id",
     name: "cardDetails",
+    props: true,
     component: CardView,
+    meta: { needLogin: true },
   },
 ];
 
@@ -32,6 +36,14 @@ const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.needLogin && !store.getters["auth/getIsLoggedIn"]) {
+    next("/login");
+  } else {
+    next();
+  }
 });
 
 export default router;
