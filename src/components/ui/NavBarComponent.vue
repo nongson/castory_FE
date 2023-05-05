de96c96a6f2c46ecba15657872b8330c9f03b35b
Internship-Thai Quang Minh <minhtq@vsii.com>
2023-05-05 15:56:49 +0700

    fix-nav-bar-and-browse-ui

--- src/components/ui/NavBarComponent.vue
+++ src/components/ui/NavBarComponent.vue
@@ -12,7 +12,12 @@
         <v-flex class="nav-login">
           <img src="@/assets/images/miniLogo.png" class="center" alt="" />
         </v-flex>
-        <v-flex class="d-flex" style="max-width: 300px" v-if="getIsLoggedIn">
+        <!--        User info and logout for SM and up-->
+        <v-flex
+          class="d-flex"
+          style="max-width: 300px"
+          v-if="getIsLoggedIn && $vuetify.breakpoint.smAndUp"
+        >
           <v-flex class="d-flex justify-end align-center">
             <img
               src="@/assets/icons/account.svg"
@@ -39,6 +44,59 @@
               style="width: 13px; height: 16px; cursor: pointer"
               @click="handleShowDialog"
             />
+          </v-flex>
+        </v-flex>
+        <!--        User info and logout for XS only-->
+        <v-flex v-if="getIsLoggedIn && $vuetify.breakpoint.xsOnly">
+          <v-flex class="d-flex justify-end align-center">
+            <v-menu
+              offset-y
+              transition="slide-y-transition"
+              location="end"
+              min-width="200"
+            >
+              <!-- -------------------Menu list-------------------- -->
+              <template v-slot:activator="{ attrs, on }">
+                <div class="d-flex align-center" v-bind="attrs" v-on="on">
+                  <img
+                    src="@/assets/icons/account.svg"
+                    alt=""
+                    style="width: 32px; height: 32px"
+                  />
+                  <img
+                    src="@/assets/icons/ArrowDown.svg"
+                    style="width: 16px; height: 16px"
+                    alt=""
+                  />
+                </div>
+              </template>
+              <v-list class="pa-0 layout-list">
+                <v-list-item
+                  class="d-flex justify-end align-center"
+                  style="border-bottom: 1px solid #0000001a"
+                >
+                  <h6>Nguyen Van A</h6>
+                </v-list-item>
+                <v-list-item>
+                  <v-flex
+                    class="d-flex justify-end align-center mb-1 logout-mobile"
+                    @click="handleShowDialog"
+                  >
+                    <captionTwo
+                      style="color: #453fe3; font-size: 14px; font-weight: 600"
+                    >
+                      Đăng xuất
+                    </captionTwo>
+                    <img
+                      src="@/assets/icons/logout-mobile.svg"
+                      alt=""
+                      class="ml-3"
+                      style="width: 13px; height: 16px"
+                    />
+                  </v-flex>
+                </v-list-item>
+              </v-list>
+            </v-menu>
           </v-flex>
         </v-flex>
       </v-col>
@@ -92,4 +150,6 @@ export default {
     width: 103px
 .nav-login
   height: 60px
+.logout-mobile
+  cursor: pointer
 </style>
